32bbf2daeaf3abd77409bddee90775bdcb67799f
Mark Carlson <carlsonmark@gmail.com>
2020-03-22 23:05:08 -0600

    Changed shared library into a python module. Had to create a static library to use for the unit tests and benchmarks, since the python library does not export symbols by default.

---
+++ src/library/pybinds.cpp
@@ -0,0 +1,15 @@
+/**
+ * Created by mark on 2020-03-22.
+ */
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+
+#include "library.h"
+#include "generated/thing1.hpp"
+#include "generated/thing2.hpp"
+
+PYBIND11_MODULE(generated_code_test, m) {
+    m.def("hello", &hello);
+}
